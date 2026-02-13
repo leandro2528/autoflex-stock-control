c4a0c80e20e45f23da3263a9e71f284301001d27
Leandro <leandrodev83@gmail.com>
2026-02-13 17:40:35 -0300

    fix: correct variables name in serve listen

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,15 +2,23 @@ const express = require("express");
 
 const cors = require("cors");
 
+const pool = require("./config/database");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Autoflex Stock Control API running"
-    });
+app.get("/", async (req, res) => {
+   try {
+    const result = await pool.query("SELECT NOW()");
+        res.json({
+            message: "AutoFlex Stock Control API running",
+            database_time: result.rows[0],
+   });
+   } catch (error){
+    res.status(500).json({ error: "Database connnection failed" });
+   }
 });
 
 module.exports = app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
 
-const PORT = precess.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(POST, () => {
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
