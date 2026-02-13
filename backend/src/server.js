5b8780090c33923d073a5e69cbc98661bf769117
Leandro <leandrodev83@gmail.com>
2026-02-13 15:51:38 -0300

    feat: configure express server setup

---
+++ backend/src/server.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const app = require("./app");
+
+const PORT = precess.env.PORT || 3000;
+
+app.listen(POST, () => {
+    console.log(`Server running on port ${PORT}`);
+});
